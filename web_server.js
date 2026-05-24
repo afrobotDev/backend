deb269d1dd240e4ec962ce14a448ac30817e9e18
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-24 16:39:46 +0300

    Add missing express.json() middleware for req.body parsing

---
+++ web_server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+let data = {
+  "name": "Kyle"
+};
+
+app.get('/', (req, res) => {
+  res.send(`
+  <body style="background: pink; color: blue;">
+  <h1>Data</h1>
+      <p>${JSON.stringify(data)}</p>
+  </body>
+  `)
+})
+
+app.get('/api/data', (req, res) => {
+  res.send(data)
+})
+
+app.post('/api/data', (req, res) => {
+  const newEntry = req.body
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
