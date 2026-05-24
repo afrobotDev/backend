adcfe7b0a742d7e1c8bbe703a2b10b87fcf2e19f
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-24 17:19:25 +0300

    Update root route to serve HTML page displaying images from static directory

--- web_server.js
+++ web_server.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.static('/Home/Downloads'));
 
 let data = {}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`
+    <body style="background: pink; color: blue;">
+    <h1>Images</h1>
+    <img src="/image.png" alt="Image" />
+    <img src="/image.jpg" alt="Image" />
+    <img src="/image.jpeg" alt="Image" />
+    <img src="/image.gif" alt="Image" />
+    <img src="/image.webp" alt="Image" />
+    </body>
+  `)
 });
 
 app.get('/users', (req, res) => {
