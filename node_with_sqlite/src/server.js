12bcbda7cad77202caa3eed0e3dd9f4414f2d733
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-25 22:18:44 +0300

    server: add Hello Mom log

--- node_with_sqlite/src/server.js
+++ node_with_sqlite/src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+console.log("Hello Mom");
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
