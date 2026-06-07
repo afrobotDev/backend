0fd90dbf7c275f28f8bef2a82782ab57ade63c72
YitbarekE <yitbarekendale.2024@gmail.com>
2026-06-07 18:33:37 +0300

    server: add static file serving and root route handler

--- node_with_sqlite/src/server.js
+++ node_with_sqlite/src/server.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log("Hello Mom");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (res, req) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
