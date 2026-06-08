8cc2589a381f08469393f01302565ff48b7b6d6e
YitbarekE <yitbarekendale.2024@gmail.com>
2026-06-08 16:22:26 +0300

    update database.js

---
+++ node_with_sqlite/src/database.js
@@ -0,0 +1,21 @@
+import { DatabaseSync } from "node:sqlite";
+
+
+const db = new DatabaseSync(':memory:');
+
+db.exec(`
+  CREATE TABLE users {
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT
+  }
+  `)
+db.exec(`
+
+  CREATE TABLE todos {
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+
+  }
+  `)
