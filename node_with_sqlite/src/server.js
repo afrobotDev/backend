e2938f4a74bd6822fc4faf5b2e2e964e58af477e
YitbarekE <yitbarekendale.2024@gmail.com>
2026-06-08 16:22:29 +0300

    update server.js

--- node_with_sqlite/src/server.js
+++ node_with_sqlite/src/server.js
@@ -1,20 +1,18 @@
-import express from 'express';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('/', (res, req) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (res, req) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
