3b4feb8a90e7e117fafdf453cd6af2791e6921fc
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-04 00:29:51 +0300

    Replace db.prepare + SELECT with prisma.user.findUnique

--- full-backend/src/routes/auth_route.js
+++ full-backend/src/routes/auth_route.js
@@ -38,11 +38,13 @@ router.post("/register", async (req, res) => {
 });
 
 // Login a user
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
-    const user = getUser.get(username);
+    const user = await prisma.user.findUnique({
+      where: { username: username },
+    });
+
     if (!user) {
       return res.status(401).json({ message: "invalid credential" });
     }
