4287055c07659613875a2e627500ec8ab7c3b4a4
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-03 05:05:18 +0300

    Replace Sqlite insert logic with Prisma

--- full-backend/src/routes/auth_route.js
+++ full-backend/src/routes/auth_route.js
@@ -6,27 +6,30 @@ import db from "../database.js";
 const router = express.Router();
 
 // Register a new user
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 8);
 
   try {
-    const insertUser = db.prepare(
-      `INSERT INTO users (username, password) VALUES (?, ?)`,
-    );
-    const result = insertUser.run(username, hashedPassword);
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
 
     const defaultTodo = "Hello, add you first todo!";
-    const insertTodo = db.prepare(
-      `INSERT INTO todos (user_id, task) VALUES (?, ?)`,
-    );
-    insertTodo.run(result.lastInsertRowid, defaultTodo);
 
-    const token = jwt.sign(
-      { id: result.lastInsertRowid },
-      process.env.JWT_SECRET,
-      { expiresIn: "24h" },
-    );
+    await prisma.todos.create({
+      data: {
+        user_id: user.id,
+        task: defaultTodo,
+      },
+    });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
     res.json({ token });
   } catch (err) {
     console.log(err.message);
