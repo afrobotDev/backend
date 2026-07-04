7d581236420aae784b67887cd3fd0350e36d247f
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-04 09:29:07 +0300

    Add JWT Bearer toeken verification

---
+++ full-backend/src/middleware/auth_middleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token =
+    authHeader && authHeader.startsWith("Bearer ") && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "no token provided" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "invalid token" });
+    }
+
+    req.userId = decoded.id;
+    next();
+  });
+}
+
+export default authMiddleware;
