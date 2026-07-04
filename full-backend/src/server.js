6d3331a6c4a7851710069dd81a26091fe0e016b9
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-04 09:28:11 +0300

    Add Express server with auth/todo routes and middlware

---
+++ full-backend/src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import authRoute from "./routes/auth_route.js";
+import todoRoute from "./routes/todo_route.js";
+import authMiddleware from "./middleware/auth_middleware.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// Routes
+app.use("/auth", authRoute);
+app.use("/todos", authMiddleware, todoRoute);
